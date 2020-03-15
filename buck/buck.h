970f29b8e0195a0bd4eb65958e07e4da11001d22
Lee Magnusson <lee@unhuman.io>
2020-03-15 16:49:52 -0700

    Work on buck for battery combiner

---
+++ buck/buck.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class PIController;
+
+// control current to a load, feedforward voltage
+class BuckController {
+ public:
+    BuckController(float dt);
+    ~BuckController();
+    // returns voltage desired
+    float step(float current_desired, float current_measured, float voltage_feedforward=0);
+ private:
+    PIController *controller_;
+};
---
+++ buck/messages_buck.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct BuckStatus {
+    float v_bus, v_out;
+    float i_bus, i_out;
+};
