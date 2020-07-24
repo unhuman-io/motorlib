c9759ce3c8aa95046c4d79ff240c258d6e9b27bb
Lee Magnusson <lee@unhuman.io>
2020-07-24 11:05:57 -0700

    register operation exclusion

--- ma732_encoder.h
+++ ma732_encoder.h
@@ -12,15 +12,19 @@ class MA732Encoder final : public SPIEncoder {
         } bits;
         uint16_t word;
     };
-    MA732Encoder(SPI_TypeDef &regs, GPIO &gpio_cs, uint8_t filter = 119) : SPIEncoder(regs, gpio_cs), filter_(filter) {}
+    MA732Encoder(SPI_TypeDef &regs, GPIO &gpio_cs, uint8_t filter = 119, volatile int *register_operation = nullptr) : SPIEncoder(regs, gpio_cs), filter_(filter) {
+        if (register_operation != nullptr) {
+            register_operation_ = register_operation;
+        }
+    }
 
     virtual void trigger()  __attribute__((section (".ccmram"))) {
-        if (!register_operation_) {
+        if (!*register_operation_) {
             SPIEncoder::trigger();
         }
     }   
     virtual int32_t read()  __attribute__((section (".ccmram"))) {
-        if (!register_operation_) {
+        if (!*register_operation_) {
             SPIEncoder::read();
             count_ += (int16_t) (data_ - last_data_); // rollover summing
             last_data_ = data_;
@@ -29,19 +33,19 @@ class MA732Encoder final : public SPIEncoder {
     }
 
     uint8_t read_register(uint8_t address) {
-        register_operation_++;
+        (*register_operation_)++;
         MA732reg reg = {};
         reg.bits.address = address;
         reg.bits.command = 0b010; // read register
         send_and_read(reg.word);
         ns_delay(750); // read register delay
         uint8_t retval = send_and_read(0) >> 8;
-        register_operation_--;
+        (*register_operation_)--;
         return retval;
     }
 
     bool set_register(uint8_t address, uint8_t value) {
-        register_operation_++;
+        (*register_operation_)++;
         bool retval = true;
         if (read_register(address) != value) {
             MA732reg reg = {};
@@ -52,7 +56,7 @@ class MA732Encoder final : public SPIEncoder {
             ms_delay(20); 
             retval = read_register(address) == value;
         }
-        register_operation_--;
+        (*register_operation_)--;
         return retval;
     }
 
@@ -78,21 +82,24 @@ class MA732Encoder final : public SPIEncoder {
 
     uint32_t get_magnetic_field_strength() {
         uint8_t original_mgt = read_register(0x6);
-        uint8_t mght = 0, mglt = 7;
+        uint8_t mght = 0, mglt = 0;
         for (uint8_t i=0; i<8; i++) {
-            set_register(0x6, i<<2 | i<<5); // increment magnetic field thresholds
+            if (!set_register(0x6, i<<2 | i<<5)) { // increment magnetic field thresholds
+                set_register(0x6, original_mgt);
+                return 0xFFFF; // a fail test value
+            }
             uint8_t test_mgt = read_register(0x1B);    
             // find last value that mght is 1 and the last that mglt is 0
             // I think they should be equal
             if (test_mgt & 0x80) {
-                mght = i;
+                mght = i+1;
             }
             if (!(test_mgt & 0x40)) {
-                mglt = i;
+                mglt = i+1;
             }
         }
         set_register(0x6, original_mgt);
-        return (mght << 2 | mglt << 5);
+        return (mght << 0 | (uint16_t) mglt << 8);
     }
 
     virtual int32_t get_value()  const __attribute__((section (".ccmram"))) { return count_; }
@@ -107,6 +114,7 @@ class MA732Encoder final : public SPIEncoder {
     uint8_t filter_;
     uint16_t last_data_ = 0;
     int32_t count_ = 0;
-    volatile int register_operation_ = 0;
+    volatile int register_operation_local_ = 0;
+    volatile int *register_operation_ = &register_operation_local_;
     uint32_t tmp;
 };
