c125b29ef9f9ceb354cb1d0d37e051968e57d9c3
Lee Magnusson <lee@unhuman.io>
2021-02-03 13:22:01 -0800

    bin param build to make script

--- scripts/firmware_installer.mk
+++ scripts/firmware_installer.mk
@@ -1,3 +1,12 @@
+$(BUILD_DIR)/%.bin: $(BUILD_DIR)/%.elf | $(BUILD_DIR)
+	$(BIN) $< $@
+	dfu-suffix -p 0x100 -v 0x3293 -a $@	
+	$(BIN) $< --remove-section flash_param $(@:.bin=_noparam.bin)
+	dfu-suffix -p 0x100 -v 0x3293 -a $(@:.bin=_noparam.bin)
+	$(BIN) $< --only-section flash_param $(@:.bin=_param_only.bin)
+	dfu-suffix -p 0x100 -v 0x3293 -a $(@:.bin=_param_only.bin)
+
+
 $(BUILD_DIR)/%.tgz: $(BUILD_DIR)/%.bin | $(BUILD_DIR)
 	rm -rf $(BUILD_DIR)/$(TARGET)
 	mkdir $(BUILD_DIR)/$(TARGET)
