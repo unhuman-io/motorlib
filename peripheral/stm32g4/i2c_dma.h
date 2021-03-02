04dc3b0028df45b318702f74655203ea6db796d4
Lee Magnusson <lee@unhuman.io>
2021-03-02 05:51:39 -0800

    i2c configurable speed

--- peripheral/stm32g4/i2c_dma.h
+++ peripheral/stm32g4/i2c_dma.h
@@ -3,13 +3,23 @@
 
 class I2C_DMA {
  public:
-    I2C_DMA(I2C_TypeDef &regs, DMA_Channel_TypeDef &tx_dma, DMA_Channel_TypeDef &rx_dma) : 
+    I2C_DMA(I2C_TypeDef &regs, DMA_Channel_TypeDef &tx_dma, DMA_Channel_TypeDef &rx_dma, uint16_t speed_khz = 100) : 
         regs_(regs), tx_dma_(tx_dma), rx_dma_(rx_dma) {
             tx_dma_.CPAR = (uint32_t) &regs_.TXDR;
-            rx_dma_.CPAR = (uint32_t) &regs_.RXDR;            
-            // regs_.TIMINGR = 0x30a0a7fb; // 100 kHz at 170 MHz clock
-            // regs_.TIMINGR = 0x10802d9b; // 400 kHz at 170 MHz clock
-            regs_.TIMINGR = 0x00802172; // 1 Mbps at 170 MHz clock
+            rx_dma_.CPAR = (uint32_t) &regs_.RXDR;
+            switch (speed_khz) {
+                case 1000:
+                    regs_.TIMINGR = 0x00802172; // 1 Mbps at 170 MHz clock
+                    break;
+                case 400:
+                    regs_.TIMINGR = 0x10802d9b; // 400 kHz at 170 MHz clock
+                    break;
+                default:
+                case 100:
+                    regs_.TIMINGR = 0x30a0a7fb; // 100 kHz at 170 MHz clock
+                    break;
+            }            
+            
             regs_.CR1 |= I2C_CR1_PE;
             regs_.CR1 |= I2C_CR1_RXDMAEN | I2C_CR1_TXDMAEN;
 
