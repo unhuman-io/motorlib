e93a80b73ad5d6f5e149513a37e2df3e72fab005
Lee Magnusson <lee@unhuman.io>
2021-03-25 06:07:11 -0700

    quadrature index checking options

--- qep_encoder.h
+++ qep_encoder.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include "encoder.h"
 #include "../st_device.h"
+#include "control_fun.h"
 
 class QEPEncoder final : public EncoderBase {
  public:
@@ -18,11 +19,26 @@ class QEPEncoder final : public EncoderBase {
    int32_t read() { value_ = regs_.CNT; return value_; } __attribute__((section (".ccmram")));
    int32_t get_value() const { return value_; } __attribute__((section (".ccmram")));
    void trigger() {} __attribute__((section (".ccmram")));
-   int32_t get_index_pos() { return regs_.CCR3; }
-   bool index_received() { return regs_.SR & TIM_SR_CC3IF; }
+   int32_t get_index_pos() { check_index(); return regs_.CCR3; }
+   bool index_received() { check_index(); return index_received_; }
+   int32_t first_index() const { return first_index_; }
+   void check_index() {
+     if(!index_received_ && regs_.SR & TIM_SR_CC3IF) {
+       index_received_ = true;
+       first_index_ = regs_.CCR3;
+     }
+   }
+   // TODO what happens in rollover?
+   int32_t index_error(int32_t cpr) {
+     int32_t diff = get_index_pos() - first_index();
+     int32_t error = (abs(diff) + cpr/2) % cpr - cpr/2;
+     return sign(diff) * error;
+   }
  private:
    TIM_TypeDef &regs_;
    int32_t value_ = 0;
+   bool index_received_ = false;
+   int32_t first_index_ = 0;
 };
 
 #endif
