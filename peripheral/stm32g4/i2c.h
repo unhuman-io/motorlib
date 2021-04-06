ec012306084c5507e635aadb9c3bdf21dad4d959
Lee Magnusson <lee@unhuman.io>
2021-04-06 13:59:00 -0700

    timing option for i2c

--- peripheral/stm32g4/i2c.h
+++ peripheral/stm32g4/i2c.h
@@ -3,7 +3,7 @@
 
 class I2C {
  public:
-    I2C(I2C_TypeDef &regs, uint16_t speed_khz = 100) : regs_(regs) {
+    I2C(I2C_TypeDef &regs, uint16_t speed_khz = 100, float timeout_ms=.1) : regs_(regs) {
             switch (speed_khz) {
                 case 1000:
                     regs_.TIMINGR = 0x00802172; // 1 Mbps at 170 MHz clock
@@ -14,8 +14,11 @@ class I2C {
                 default:
                 case 100:
                     regs_.TIMINGR = 0x30a0a7fb; // 100 kHz at 170 MHz clock
+                    // 0xfb << I2C_TIMINGR_SCLL_Pos | 0xa7 << I2C_TIMINGR_SCLH_Pos | 0xa << I2C_TIMINGR_SCLDEL_Pos | 3 << I2C_TIMINGR_PRESC_Pos;
+                    // 42.5 MHz, 5.9us low, 3.9 us high, .26 us low stetch
                     break;
-            }       
+            }
+            timeout_ms_ = timeout_ms;       
             regs_.CR1 |= I2C_CR1_PE;
     }
     void write(uint8_t address, int8_t nbytes, uint8_t *data, bool stop = false) {
@@ -26,7 +29,7 @@ class I2C {
 
         for (int i=1; i<nbytes; i++) {
             uint32_t t_start = get_clock();
-            while_timeout_ms(!tx_ready() && !trouble(), .1);
+            while_timeout_ms(!tx_ready() && !trouble(), timeout_ms_);
             if(trouble()) {
                 clear_isr();
                 regs_.CR2 = 0;
@@ -35,7 +38,7 @@ class I2C {
             regs_.TXDR = data[i];
         }
         uint32_t t_start = get_clock();
-        while_timeout_ms(!transfer_complete() && !trouble(), .2);
+        while_timeout_ms(!transfer_complete() && !trouble(), timeout_ms_);
         if(trouble()) {
             clear_isr();
             regs_.CR2 = 0;
@@ -52,7 +55,7 @@ class I2C {
 
         for(int i=0;i<nbytes; i++) {
             uint32_t t_start = get_clock();
-            while_timeout_ms(!rx_ready() && !trouble(), .1);
+            while_timeout_ms(!rx_ready() && !trouble(), timeout_ms_);
             if(trouble()) {
                 clear_isr();
                 return;
@@ -60,7 +63,7 @@ class I2C {
             data[i] = regs_.RXDR;
         }
         uint32_t t_start = get_clock();
-        while_timeout_ms(!transfer_complete(), .2);
+        while_timeout_ms(!transfer_complete(), timeout_ms_);
         regs_.CR2 = I2C_CR2_STOP;
     }
     bool tx_ready() const {
@@ -79,5 +82,6 @@ class I2C {
     void clear_isr() {
         regs_.ICR |= 0x3F38;
     }
+    float timeout_ms_;
     I2C_TypeDef &regs_;
 };
