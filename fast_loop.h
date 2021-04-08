79e0c9dad9da41aa7af8531b550f9410e0d645c2
Lee Magnusson <lee@unhuman.io>
2021-04-08 12:04:01 -0700

    fix stepper slow speed

--- fast_loop.h
+++ fast_loop.h
@@ -83,7 +83,7 @@ class FastLoop {
         foc_command_.measured.motor_encoder = stepper_position_;
         motor_position_filtered_ = stepper_position_;
         stepper_position_ += stepper_velocity_ * dt_;
-        stepper_position_ = wrap1(stepper_position_, 2*M_PI*1000);
+        stepper_position_ = wrap1(stepper_position_, 2*M_PI);
       }
       
       FOCStatus *foc_status = foc_->step(foc_command_);
