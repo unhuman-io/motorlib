2c00dfac6fd559ec57c2fb2e952e99dc1ec6f305
Lee Magnusson <lee@unhuman.io>
2021-04-29 10:43:33 -0700

    update sbank script

--- scripts/set_sbank.sh
+++ scripts/set_sbank.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-dfu-util -a1 -s 0x1FFF7800:16 -D $(dirname $0)/sbank.bin $@
+fname=sbank.bin
+if [ $1 == "-c" ]; then
+fname=dbank.bin
+shift
+fi
+echo $fname
+dfu-util -a1 -s 0x1FFF7800:8 -D $(dirname $0)/$fname $@
