a8ddd661acfeba51d29e29a9d1efaaf35e6a91b8
Lee Magnusson <lee@unhuman.io>
2021-06-02 13:05:51 -0700

    add debug features to ads1235

--- util.h
+++ util.h
@@ -40,6 +40,8 @@ inline uint32_t get_heap_used() {
 #define while_timeout_ms(condition, ms) while((condition) && ((get_clock() - t_start) < ms*CPU_FREQUENCY_HZ/1000))
 
 #ifdef __cplusplus
+#include <string>
+#include <vector>
 class FrequencyLimiter {
  public:
     FrequencyLimiter(float rate_seconds) {
@@ -65,6 +67,36 @@ inline T signextend(const T x)
   struct {T x:B;} s;
   return s.x = x;
 }
+
+inline std::vector<char> hex_to_bytes(const std::string& hex) {
+  std::vector<char> bytes;
+
+  for (unsigned int i = 0; i < hex.length(); i += 2) {
+    std::string byteString = hex.substr(i, 2);
+    char byte = (char) strtol(byteString.c_str(), NULL, 16);
+    bytes.push_back(byte);
+  }
+  return bytes;
+}
+
+inline std::string byte_to_hex(const uint8_t byte) {
+    const char hexval[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    char c[3] = { hexval[(byte >> 4) & 0xF], hexval[byte & 0xF] };
+    return c;
+}
+
+inline std::string u16_to_hex(const uint16_t w) {
+    return byte_to_hex((uint8_t) (w>>8)) + byte_to_hex((uint8_t) (w & 0xFF));
+}
+
+inline std::string bytes_to_hex(const std::vector<char>& bytes) { 
+    std::string s;   
+    for (uint8_t b : bytes) {
+        s += byte_to_hex(b);
+    }
+    return s;
+}
+
 #endif
 
 #endif
