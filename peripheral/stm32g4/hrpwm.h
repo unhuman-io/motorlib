bc4a109eb7ba0a5ca454cfd152c4e0e38af819f2
Lee Magnusson <lee@unhuman.io>
2021-10-06 14:09:43 -0700

    more updates hrpwm deadtime

--- peripheral/stm32g4/hrpwm.h
+++ peripheral/stm32g4/hrpwm.h
@@ -27,10 +27,11 @@ class HRPWM final : public PWMBase {
          regs_.sTimerxRegs[ch].TIMxCR2 = HRTIM_TIMCR2_UDM;
          if (pwm3_mode_) {
             regs_.sTimerxRegs[ch].SETx2R = HRTIM_SET2R_SST;
-         } else {
-            set_deadtime(ch, deadtime_ns_);
          }
          regs_.sTimerxRegs[ch].TIMxCR = HRTIM_TIMCR_PREEN | HRTIM_TIMCR_TRSTU | HRTIM_TIMCR_CONT;
+      }
+      if (!pwm3_mode_) {
+         set_deadtime(deadtime_ns_);
       }
       MASK_SET(regs_.sTimerxRegs[5].TIMxCR2, HRTIM_TIMCR2_ADROM, 1);   // adc event generated at 0 on F
       regs_.sCommonRegs.DLLCR = HRTIM_DLLCR_CALEN | (3 << HRTIM_DLLCR_CALRTE_Pos); // periodic calibration at 2048*hrtim = 12us
@@ -43,11 +44,14 @@ class HRPWM final : public PWMBase {
    void open_mode();
    void brake_mode();
    void voltage_mode();
-   void set_deadtime(uint8_t ch, uint16_t deadtime_ns) {
+   void set_deadtime(uint16_t deadtime_ns) {
+      deadtime_ns_ = deadtime_ns;
       uint32_t deadprescale = 0;
       uint32_t deadtime = deadtime_ns_ * count_per_ns_; // 9 bits at 170e6*32/4 gives 376 ns
-      regs_.sTimerxRegs[ch].OUTxR = HRTIM_OUTR_DTEN;
-      regs_.sTimerxRegs[ch].DTxR = (deadtime << HRTIM_DTR_DTF_Pos) | (deadtime << HRTIM_DTR_DTR_Pos) | (deadprescale << HRTIM_DTR_DTPRSC_Pos);
+      for(auto ch : std::vector<uint8_t>{ch_a_, ch_b_, ch_c_}) {
+         regs_.sTimerxRegs[ch].OUTxR = HRTIM_OUTR_DTEN;
+         regs_.sTimerxRegs[ch].DTxR = (deadtime << HRTIM_DTR_DTF_Pos) | (deadtime << HRTIM_DTR_DTR_Pos) | (deadprescale << HRTIM_DTR_DTPRSC_Pos);
+      }
    }
    void set_frequency_hz(uint32_t frequency_hz, uint16_t min_off_ns = 0, uint16_t min_on_ns = 0);
  private:
@@ -61,6 +65,8 @@ class HRPWM final : public PWMBase {
    float pwm_min_ = 0;
    float pwm_max_;
    const float count_per_ns_ = CPU_FREQUENCY_HZ * 32 / 4 / 1.e9; // Datasheet says /8 not /4, but /4 seems to give correct scale
+
+   friend void config_init();
 };
 
 #endif
