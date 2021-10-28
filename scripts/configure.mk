6308727d9271c47ccf7552982021058fd3abb1ec
Lee Magnusson <lee@unhuman.io>
2021-10-28 12:56:40 -0700

    break up makefile

---
+++ scripts/configure.mk
@@ -0,0 +1,90 @@
+#######################################
+# paths
+#######################################
+# Requires PARAM_FILE, CONFIG_FILE, and TARGET_MCU defined
+
+# Build path
+BUILD_DIR = build
+GIT_VERSION := $(shell git describe --dirty --always --tags)
+
+ifndef TARGET_MCU
+$(error need to define TARGET_MCU)
+endif
+
+$(info target is $(TARGET_MCU))
+
+ifeq "$(TARGET_MCU)" "stm32g474"
+
+######################################
+# source
+######################################
+# C sources
+C_SOURCES =  \
+Src/main.c \
+Src/stm32g4xx_it.c \
+Src/system_stm32g4xx.c \
+$(DRIVERS)/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c \
+$(DRIVERS)/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c \
+$(DRIVERS)/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c \
+$(DRIVERS)/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c \
+$(DRIVERS)/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c \
+$(DRIVERS)/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c \
+
+
+
+# ASM sources
+ASM_SOURCES =  \
+../motorlib/peripheral/stm32g4/startup_stm32g474xx.s
+
+
+# macros for gcc
+# AS defines
+AS_DEFS = 
+
+# C defines
+override C_DEFS +=  \
+-DGIT_VERSION=\"$(GIT_VERSION)\" \
+-DUSE_HAL_DRIVER \
+-DSTM32G474xx
+
+# AS includes
+AS_INCLUDES = 
+
+# C includes
+C_INCLUDES =  \
+-IInc \
+-IDrivers/CMSIS/Include \
+-I$(DRIVERS)/STM32G4xx_HAL_Driver/Inc \
+-I$(DRIVERS)/STM32G4xx_HAL_Driver/Inc/Legacy \
+-I$(DRIVERS)/CMSIS/Device/ST/STM32G4xx/Include \
+-I$(DRIVERS)/CMSIS/Include
+
+#######################################
+# LDFLAGS
+#######################################
+# link script
+LDSCRIPT = ../motorlib/peripheral/stm32g4/STM32G474RETx_FLASH.ld
+
+DRIVERS = ../boost_g474/Drivers
+
+endif # MCU_TARGET
+
+CPP_SOURCES = \
+../motorlib/control_fun.cpp\
+foc.cpp\
+gpio.cpp\
+sincos.cpp\
+util.cpp\
+hall.cpp\
+../motorlib/peripheral/spi_encoder.cpp\
+../motorlib/peripheral/stm32g4/usb.cpp\
+hrpwm.cpp\
+ams_encoder.cpp\
+parameter_api.cpp\
+./$(CONFIG_FILE)
+
+C_SOURCES += ./$(PARAM_FILE)
+
+ifdef PARAM_OVERRIDE
+PARAM_INCLUDE=-include $(PARAM_OVERRIDE)
+endif
---
+++ scripts/make_param.mk
@@ -0,0 +1,15 @@
+ifdef PARAM_OVERRIDE
+PARAM_INCLUDE=-include $(PARAM_OVERRIDE)
+endif
+
+PARAM_BUILD_DIR = $(BUILD_DIR)/$(dir $(PARAM_FILE))
+
+all:: param
+param: $(BUILD_DIR)/$(PARAM_FILE:c=bin)
+
+$(BUILD_DIR)/$(PARAM_FILE:c=bin): $(PARAM_FILE) $(PARAM_OVERRIDE) | $(BUILD_DIR) $(PARAM_BUILD_DIR)
+	$(CC) $(PARAM_INCLUDE) -c $< -o $(BUILD_DIR)/$(<:c=o)
+	$(CP) -O binary -S -j flash_param $(BUILD_DIR)/$(<:c=o) $@ 
+
+$(PARAM_BUILD_DIR):
+	mkdir -p $@
