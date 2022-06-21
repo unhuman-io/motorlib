9def40139b7550f4cb4c185c77bd3b80dfdfa4cc
Lee Magnusson <lee@unhuman.io>
2022-06-20 19:30:58 -0700

    lower vbus min

--- main_loop.h
+++ main_loop.h
@@ -35,7 +35,7 @@ class MainLoop {
           output_encoder_correction_table_(param_.output_encoder.table), brake_(brake) {
           set_param(param);
           if (param_.vbus_min == 0) {
-            param_.vbus_min = 9;  // defaults that can be overridden via api
+            param_.vbus_min = 8;  // defaults that can be overridden via api
           }
           if (param_.vbus_max == 0) {
             param_.vbus_max = 60;
@@ -390,7 +390,7 @@ class MainLoop {
     TrajectoryGenerator position_trajectory_generator_;
     uint32_t timestamp_ = 0;
     uint32_t last_timestamp_ = 0;
-    uint32_t *reserved0_ = &timestamp_;
+    uint32_t *reserved0_ = reinterpret_cast<uint32_t *>(&status_.fast_loop.vbus);
     uint32_t *reserved1_ = &timestamp_;
     uint32_t *reserved2_ = &last_timestamp_;
     PChipTable<OUTPUT_ENCODER_TABLE_LENGTH> output_encoder_correction_table_;
