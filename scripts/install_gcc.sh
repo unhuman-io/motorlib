df5bb0e456a3607589ca44a5b1caa7e408a6cfa5
Lee Magnusson <leemagnusson@gmail.com>
2022-10-07 15:27:46 -0700

    update gcc version

--- scripts/install_gcc.sh
+++ scripts/install_gcc.sh
@@ -6,13 +6,16 @@ gcc_dir=$(dirname $0)/../gcc
 mkdir -p $gcc_dir
 cd $gcc_dir
 
-version=gcc-arm-11.2-2022.02
-arch=$(uname -m)
-fname=$version-$arch-arm-none-eabi
-declare -A sha=( [x86_64]=8c5acd5ae567c0100245b0556941c237369f210bceb196edfe5a2e7532c60326 \
-                 [aarch64]=ef1d82e5894e3908cb7ed49c5485b5b95deefa32872f79c2b5f6f5447cabf55f )
+# version=11.2-2022.02
+# declare -A sha=( [x86_64]=8c5acd5ae567c0100245b0556941c237369f210bceb196edfe5a2e7532c60326 \
+#                  [aarch64]=ef1d82e5894e3908cb7ed49c5485b5b95deefa32872f79c2b5f6f5447cabf55f )
+version=11.3.rel1
+declare -A sha=( [x86_64]=d420d87f68615d9163b99bbb62fe69e85132dc0a8cd69fca04e813597fe06121 \
+                 [aarch64]=fb9e562a90de1b3a2961b952193c1c6520872aa1482c0a5e0ab79970ec6e7690 )
 
-url=https://developer.arm.com/-/media/Files/downloads/gnu/11.2-2022.02/binrel/$fname.tar.xz
+arch=$(uname -m)
+fname=arm-gnu-toolchain-$version-$arch-arm-none-eabi
+url=https://developer.arm.com/-/media/Files/downloads/gnu/$version/binrel/$fname.tar.xz
 wget $url
 cat <(printf "${sha[$arch]} $fname.tar.xz\n")
 sha256sum -c <(printf "${sha[$arch]} $fname.tar.xz\n")
