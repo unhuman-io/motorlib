4a6bde93c760c4565da6530075bb374264c4a7d3
Lee Magnusson <lee@unhuman.io>
2023-01-09 13:48:23 -0800

    clean up uart

--- peripheral/stm32g4/qia128_uart.h
+++ peripheral/stm32g4/qia128_uart.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../../torque_sensor.h"
 #include "../../logger.h"
+#include <initializer_list>
 
 static uint8_t crc8(uint8_t *p);
 
@@ -8,72 +9,39 @@ class QIA128_UART : public TorqueSensorBase {
  public:
     QIA128_UART(USART_TypeDef &regs) : regs_(regs) {}
     bool init() {
-        for (uint8_t b : {0, 6, 0, 0x0c, 0, 0x3c}) {
-            regs_.TDR = b;
-        }
+        uart_tx({0, 6, 0, 0x0c, 0, 0x3c});
 
         ms_delay(100);
-        while(regs_.ISR & USART_ISR_RXNE) {
-            regs_.RDR;
-        }
+        clear_uart_receive_buffer();
 
-        for (uint8_t b : {0, 7, 3, 0x19, 0, 0, 0x7b}) {
-            regs_.TDR = b;
-        }
+        uart_tx({0, 7, 3, 0x19, 0, 0, 0x7b});
 
-        for (int i=0; i<4; i++) {
-            while(!(regs_.ISR & USART_ISR_RXNE));
-            regs_.RDR;
-            
-        }
-        for (int i=0; i<4; i++) {
-            while(!(regs_.ISR & USART_ISR_RXNE));
-            offset_ |= regs_.RDR << 8*(3-i);
-        }
-        while(!(regs_.ISR & USART_ISR_RXNE));
-        regs_.RDR;
+        uart_rx_uint32();
+        offset_ = uart_rx_uint32();
+        uart_rx_uint8();
         
         ms_delay(10);
 
-        for (uint8_t b : {0, 7, 3, 0x19, 0, 5, 0x99}) {
-            regs_.TDR = b;
-        }
+        uart_tx({0, 7, 3, 0x19, 0, 5, 0x99});
 
-        for (int i=0; i<4; i++) {
-            while(!(regs_.ISR & USART_ISR_RXNE));
-            regs_.RDR;
-            
-        }
-        for (int i=0; i<4; i++) {
-            while(!(regs_.ISR & USART_ISR_RXNE));
-            full_scale_ |= regs_.RDR << 8*(3-i);
-        }
-        while(!(regs_.ISR & USART_ISR_RXNE));
-        regs_.RDR;
+        uart_rx_uint32();
+        full_scale_ uart_rx_uint32();
+        uart_rx_uint8();
 
         logger.log_printf("qia128 offset: %d, full scale: %d", offset_, full_scale_);
         full_scale_ = 16777215;
         offset_ = 8356324;
 
         ms_delay(10);
-        for (uint8_t b : {0, 7, 4, 0x1e, 0, 7, 0xbc}) {
-            regs_.TDR = b;
-        }
-        
-        for (int i=0; i<5; i++) {
-            while(!(regs_.ISR & USART_ISR_RXNE));
-            regs_.RDR;
-        }
+        uart_tx({0, 7, 4, 0x1e, 0, 7, 0xbc});
+        uart_rx_uint8();
+        uart_rx_uint32();
 
         ms_delay(100);
-        for (uint8_t b : {0, 6, 0, 0xc, 1, 0x41}) {
-            regs_.TDR = b;
-        }
+        uart_tx({0, 6, 0, 0xc, 1, 0x41});
+        uart_rx_uint8();
+        uart_rx_uint32();
 
-        for (int i=0; i<5; i++) {
-            while(!(regs_.ISR & USART_ISR_RXNE));
-            regs_.RDR;
-        }
 
         regs_.CR3 = 2 << USART_CR3_RXFTCFG_Pos; // 4 bytes fifo threshold
 
@@ -86,11 +54,37 @@ class QIA128_UART : public TorqueSensorBase {
         return true;
     }
 
+    // fifo size 8 bytes
+    void uart_tx(std::initializer_list<uint8_t> data) {
+        for (uint8_t b : data) {
+            regs_.TDR = b;
+        }
+    }
+
+    uint8_t uart_rx_uint8() {
+        wait_while_false_with_timeout_us(regs_.ISR & USART_ISR_RXNE, 1000);
+        return regs_.RDR;
+    }
+
+    uint32_t uart_rx_uint32() {
+        uint32_t value = 0;
+        for (int i=0; i<4; i++) {
+            value |= uart_rx_uint8() << 8*(3-i);
+        }
+        return value;
+    }
+
+    void clear_uart_receive_buffer() {
+        while(regs_.ISR & USART_ISR_RXNE) {
+            regs_.RDR;
+        }
+    }
+
     float read() {
         if (state_ == WAIT) {
             // read character
             if (regs_.ISR & USART_ISR_RXNE) {
-                regs_.RQR = USART_RQR_RXFRQ;
+                regs_.RQR = USART_RQR_RXFRQ; // maybe switch to clear
                 wait_count_ = 0;
                 wait_error_++;
             } else {
@@ -105,7 +99,7 @@ class QIA128_UART : public TorqueSensorBase {
                 read_len_ = 0;
                 // 4 bytes received
                 for (int i=0; i<3; i++) {
-                    if (!(regs_.ISR & USART_ISR_RXNE)) {
+                    if (!(regs_.ISR & USART_ISR_RXNE)) { // move to read uint32 or 24
                         read_error_++;
                     } else {
                         read_len_++;
