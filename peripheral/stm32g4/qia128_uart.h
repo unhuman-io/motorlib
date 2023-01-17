2383581586fc0d28173c1996156eb591653b3025
Lee Magnusson <lee@unhuman.io>
2023-01-17 12:18:22 -0800

    some more uart

--- peripheral/stm32g4/qia128_uart.h
+++ peripheral/stm32g4/qia128_uart.h
@@ -9,36 +9,50 @@ class QIA128_UART : public TorqueSensorBase {
  public:
     QIA128_UART(USART_TypeDef &regs) : regs_(regs) {}
     bool init() {
-        uart_tx({0, 6, 0, 0x0c, 0, 0x3c});
+
+        //uart_tx({0, 5, 0, 1, 0xE}); // check for loopback on this value
+
+        //ms_delay(100);
+        uart_tx({0, 6, 0, 0x0c, 0, 0x3c}); // set stream state off
 
         ms_delay(100);
         clear_uart_receive_buffer();
 
-        uart_tx({0, 7, 3, 0x19, 0, 0, 0x7b});
+        uart_tx({0, 7, 3, 0x19, 0, 0, 0x7b}); // get adc cal 0
 
-        uart_rx_uint32();
+        uart_rx_uint32(); // 0,9,3,19
         offset_ = uart_rx_uint32();
-        uart_rx_uint8();
+        uart_rx_uint8(); // checksum
         
         ms_delay(10);
+        
+        uint8_t crc[] = {0x7b, 0x81, 0x87, 0x8D, 0x93, 0x99, 0x9F, 0xA5, 0xAB, 0xB1,
+                0xB7, 0xBD, 0xC3, 0xC9};
+        for (uint8_t i=0; i<13; i++) {
+            uart_tx({0, 7, 3, 0x19, 0, i, crc[i]}); // get adc cal 1
 
-        uart_tx({0, 7, 3, 0x19, 0, 5, 0x99});
+            uart_rx_uint32();
+            uint32_t tmp = uart_rx_uint32();
+            uart_rx_uint8();
+            logger.log_printf("cal%d: %d",i, tmp);
+            ms_delay(10);
+            IWDG->KR = 0xAAAA;
+        }        
 
+        uart_tx({0, 7, 3, 0x19, 0, 5, 0x99}); // get adc cal 5
         uart_rx_uint32();
-        full_scale_ uart_rx_uint32();
+        full_scale_ = uart_rx_uint32();
         uart_rx_uint8();
 
         logger.log_printf("qia128 offset: %d, full scale: %d", offset_, full_scale_);
-        full_scale_ = 16777215;
-        offset_ = 8356324;
 
         ms_delay(10);
-        uart_tx({0, 7, 4, 0x1e, 0, 7, 0xbc});
+        uart_tx({0, 7, 4, 0x1e, 0, 7, 0xbc});   // set sampling rate 1300 sps
         uart_rx_uint8();
         uart_rx_uint32();
 
         ms_delay(100);
-        uart_tx({0, 6, 0, 0xc, 1, 0x41});
+        uart_tx({0, 6, 0, 0xc, 1, 0x41});       // set stream state on
         uart_rx_uint8();
         uart_rx_uint32();
 
@@ -62,7 +76,7 @@ class QIA128_UART : public TorqueSensorBase {
     }
 
     uint8_t uart_rx_uint8() {
-        wait_while_false_with_timeout_us(regs_.ISR & USART_ISR_RXNE, 1000);
+        wait_while_false_with_timeout_us(regs_.ISR & USART_ISR_RXNE, 10000);
         return regs_.RDR;
     }
 
@@ -78,6 +92,7 @@ class QIA128_UART : public TorqueSensorBase {
         while(regs_.ISR & USART_ISR_RXNE) {
             regs_.RDR;
         }
+        regs_.RQR = USART_RQR_RXFRQ;
     }
 
     float read() {
@@ -110,6 +125,7 @@ class QIA128_UART : public TorqueSensorBase {
                 if (regs_.ISR & USART_ISR_RXNE) {
                     read_len_++;
                     crc_read_ = regs_.RDR;
+                    full_raw_ = raw_<<8 | crc_read_;
                     if (crc8(raw_bytes_) != crc_read_) {
                         crc_calc_ = crc8(raw_bytes_);
                         crc_error_++;
@@ -124,7 +140,7 @@ class QIA128_UART : public TorqueSensorBase {
                 }
                 regs_.RQR = USART_RQR_RXFRQ;
                 regs_.ISR &= ~USART_ISR_RXFT;
-                torque_ = (float) ((int32_t) raw_- (int32_t) offset_)/(full_scale_ - offset_)*25;
+                torque_ = (float) ((int32_t) raw_- (int32_t) offset_)/(full_scale_ - offset_)*gain_ + bias_;
                 state_ = WAIT;
             }
         }
@@ -142,6 +158,7 @@ class QIA128_UART : public TorqueSensorBase {
     uint32_t read_len_ = 0;
     uint8_t crc_read_ = 0;
     uint8_t crc_calc_ = 0;
+    uint32_t full_raw_ = 0;
  private:
     volatile USART_TypeDef& regs_;
     enum State {WAIT, READ} state_ = WAIT;
@@ -151,5 +168,5 @@ class QIA128_UART : public TorqueSensorBase {
 
 // From direct communications with Futek
 uint8_t crc8(uint8_t *p) {
-    return p[0]*1 + p[1]*2 + p[3]*3;
+    return p[0]*1 + p[1]*2 + p[2]*3;
 }
