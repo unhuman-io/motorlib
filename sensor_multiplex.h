e6c7ea9bf13b820abcc09a4902bda1ac734329ad
Jeff Bingham <bingjeff@gmail.com>
2023-03-19 20:58:43 -0700

    Clean-up sensor multiplex.

--- sensor_multiplex.h
+++ sensor_multiplex.h
@@ -4,7 +4,7 @@
 #include "sensor.h"
 #include "torque_sensor.h"
 
-// alternates reads between two sensors. Calling SensorMultiplex functions
+// Alternates reads between two sensors. Calling SensorMultiplex functions
 // alternate between triggering the primary and secondary reads. It returns the
 // primary sensor values Using secondary() returns a SecondarySensor functions
 // only returns values
@@ -14,9 +14,9 @@ class SensorMultiplex : public SensorBase {
   class SecondarySensor : public SensorBase {
    public:
     SecondarySensor(Sensor2 *secondary) : secondary_(secondary) {}
-    int32_t get_value() const { return secondary_->get_value(); }
-    int32_t read() { return get_value(); }
     bool init() { return secondary_->init(); }
+    int32_t read() { return get_value(); }
+    int32_t get_value() const { return secondary_->get_value(); }
     bool index_received() const { return secondary_->index_received(); }
 
    private:
@@ -28,18 +28,8 @@ class SensorMultiplex : public SensorBase {
         secondary_(secondary),
         secondary_read_(&secondary),
         decimation_(decimation) {}
-  SecondarySensor &secondary() { return secondary_read_; }
-  int32_t read() {
-    if (count_ == 0) {
-      if (toggle_)
-        primary_.read();
-      else
-        secondary_.read();
-      toggle_ = !toggle_;
-    }
-    return primary_.get_value();
-  }
-  int32_t get_value() const { return primary_.get_value(); }
+
+  bool init() { return primary_.init(); }
   void trigger() {
     if (count_++ > decimation_) {
       count_ = 0;
@@ -52,9 +42,21 @@ class SensorMultiplex : public SensorBase {
       }
     }
   }
+  int32_t read() {
+    if (count_ == 0) {
+      if (toggle_)
+        primary_.read();
+      else
+        secondary_.read();
+      toggle_ = !toggle_;
+    }
+    return primary_.get_value();
+  }
+  int32_t get_value() const { return primary_.get_value(); }
+
   int32_t get_index_pos() const { return primary_.get_index_pos(); }
   bool index_received() const { return primary_.index_received(); }
-  bool init() { return primary_.init(); }
+  SecondarySensor &secondary() { return secondary_read_; }
 
  protected:
   Sensor1 &primary_;
@@ -78,9 +80,6 @@ class TorqueSensorMultiplex : public SensorMultiplex<Sensor1, Sensor2> {
         torque_(primary.torque_) {}
   using SensorMultiplex<Sensor1, Sensor2>::trigger;
   using SensorMultiplex<Sensor1, Sensor2>::init;
-  void set_param(const TorqueSensorParam &param) {
-    this->primary_.set_param(param);
-  }
   float read() {
     if (this->count_ == 0) {
       if (this->toggle_)
@@ -91,11 +90,16 @@ class TorqueSensorMultiplex : public SensorMultiplex<Sensor1, Sensor2> {
     }
     return this->primary_.get_value();
   }
-
   float get_value() const { return this->primary_.get_value(); }
+  void set_param(const TorqueSensorParam &param) {
+    this->primary_.set_param(param);
+  }
 
  protected:
-  float &gain_, &bias_, &k_temp_, &torque_;
+  float &gain_;
+  float &bias_;
+  float &k_temp_;
+  float &torque_;
   friend class System;
 };
 
