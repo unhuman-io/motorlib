362e28c6d368def0afd2f1ae4d242b062ff20506
Lee Magnusson <lee@unhuman.io>
2023-05-03 16:08:27 -0700

    fix pt1000

--- temperature_sensor.h
+++ temperature_sensor.h
@@ -17,10 +17,10 @@ class PT1000 : public TemperatureSensor {
         float voltage = 1.0/4096*adc_;
         // top side 1k/1k
         float resistance = 1000*(3.3/voltage-1);
-        temperature_ = (resistance-1000)/(resistance*3.91e-3);
+        temperature_ = (resistance-1000)/(1000*3.91e-3);
         return get_temperature(); 
     }
-    float get_temperature() const { return temperature_; }
+    float get_temperature() const { return temperature_; }   
  private:
     volatile uint32_t& adc_;
     float temperature_ = 0;
