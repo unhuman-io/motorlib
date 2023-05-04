e0b36533ac2ae0a8650c8509bca408d2c1c44d93
Lee Magnusson <lee@unhuman.io>
2023-05-04 08:56:32 -0700

    better divider accuracy on pt1000

--- temperature_sensor.h
+++ temperature_sensor.h
@@ -12,17 +12,18 @@ class TemperatureSensor {
 
 class PT1000 : public TemperatureSensor {
  public:
-    PT1000(volatile uint32_t& adc) : adc_(adc) {}
+    PT1000(volatile uint32_t& adc, float v3v3 = 3.3) : adc_(adc), v3v3_(v3v3) {}
     float read() {
         float voltage = 1.0/4096*adc_;
         // top side 1k/1k
-        float resistance = 1000*(3.3/voltage-1);
+        float resistance = 1000*(v3v3_/voltage-1);
         temperature_ = (resistance-1000)/(1000*3.91e-3);
         return get_temperature(); 
     }
     float get_temperature() const { return temperature_; }   
  private:
     volatile uint32_t& adc_;
+    float v3v3_;
     float temperature_ = 0;
 };
 
