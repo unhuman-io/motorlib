9fb40fdd2dd407bcd38e0a7a2fcc0827edbd4578
Lee Magnusson <lee@unhuman.io>
2023-08-14 10:25:50 -0700

    Fix temperature model

--- temperature_model.h
+++ temperature_model.h
@@ -1,22 +1,22 @@
 #pragma once
 #include <cmath>
 
-// Tdot = 1/Cp*(i^2*r - T*Rth)
-// Ad = exp(-Rth/Cp*dt)
-// Bd = -1/Rth*(Ad - 1)
+// Tdot = 1/Cp*(i^2*r - T/Rth)
+// Ad = exp(-1/(Rth*Cp)*dt)
+// Bd = -Rth*(Ad - 1)
 class TemperatureModel {
  public:
     TemperatureModel() {}
     float step(float id, float iq) {
-        temperature_ = Ad_ * temperature_ + Bd_ * (id * id + iq * iq) + temperature_ambient_;
+        temperature_ = Ad_ * temperature_ + Bd_ * (id * id + iq * iq);
         return get_value();
     }
-    float get_value() const { return temperature_; }
+    float get_value() const { return temperature_  + temperature_ambient_; }
     void set_ambient_temperature(float temp) { temperature_ambient_ = temp; }
     void set_param(float resistance, float Cp, float Rth, float dt) {
-        resistance_ = resistance;
-        Ad_ = std::exp(-Rth/Cp*dt);
-        Bd_ = -1/Rth*(Ad_ - 1);
+        resistance_ = resistance*.75; // line to line to phase
+        Ad_ = std::exp(-1/(Rth*Cp)*dt);
+        Bd_ = -Rth*(Ad_ - 1);
     }
  private:
     float temperature_ = 0;
