4103478f7efb7f35d50ca456d16bf13163c27b1d
Lee Magnusson <lee@unhuman.io>
2024-01-17 12:41:10 -0800

    reorganize dfu

--- make/firmware_installer.mk
+++ make/firmware_installer.mk
@@ -7,7 +7,13 @@ $(BUILD_DIR)/%.bin: $(BUILD_DIR)/%.elf | $(BUILD_DIR)
 	-dfu-suffix -p 0x100 -v 0x3293 -a $(@:.bin=_param_only.bin)
 
 define dfu_leave =
-tmp=\$$(mktemp) && rm \$$tmp && dfu-util -a0 -s 0x8000000:8:leave -U \$$tmp || true; rm \$$tmp
+tmp=\$$(mktemp) && rm \$$tmp && dfu-util -a0 -s 0x8000000:8:leave -U \$$tmp \$$@ || true; rm \$$tmp
+endef
+define dfu_param =
+dfu-util -a0 -s 0x8060000 -D \$$(dirname \$$0)/$(notdir $(<:.bin=_param_only.bin)) \$$@
+endef
+define dfu_firmware =
+dfu-util -a0 -s 0x8000000 -D \$$(dirname \$$0)/$(notdir $(<:.bin=_noparam.bin)) \$$@
 endef
 
 $(BUILD_DIR)/%.tgz: $(BUILD_DIR)/%.bin | $(BUILD_DIR)
@@ -15,9 +21,9 @@ $(BUILD_DIR)/%.tgz: $(BUILD_DIR)/%.bin | $(BUILD_DIR)
 	mkdir $(BUILD_DIR)/$(TARGET)
 	cp $(<:.bin=_noparam.bin) $(BUILD_DIR)/$(TARGET)
 	cp $(<:.bin=_param_only.bin) $(BUILD_DIR)/$(TARGET)
-	echo "#!/bin/bash -e\n$(DFU_START_LINE)\ndfu-util -a0 -s 0x8000000 -D \$$(dirname \$$0)/$(notdir $(<:.bin=_noparam.bin)) \$$@\ndfu-util -a0 -s 0x8060000 -D \$$(dirname \$$0)/$(notdir $(<:.bin=_param_only.bin)) \$$@\n$(dfu_leave)" > $(BUILD_DIR)/$(TARGET)/load_$(TARGET).sh
+	echo "#!/bin/bash -e\n$(DFU_START_LINE)\n$(dfu_firmware)\n$(dfu_param)\n$(dfu_leave)" > $(BUILD_DIR)/$(TARGET)/load_$(TARGET).sh
 	chmod +x $(BUILD_DIR)/$(TARGET)/load_$(TARGET).sh
-	echo "#!/bin/bash -e\n$(DFU_START_LINE)\ndfu-util -a0 -s 0x8060000 -D \$$(dirname \$$0)/$(notdir $(<:.bin=_param_only.bin)) \$$@\n$(dfu_leave)" > $(BUILD_DIR)/$(TARGET)/load_$(TARGET)_param.sh
+	echo "#!/bin/bash -e\n$(DFU_START_LINE)\n$(dfu_param)\n$(dfu_leave)" > $(BUILD_DIR)/$(TARGET)/load_$(TARGET)_param.sh
 	chmod +x $(BUILD_DIR)/$(TARGET)/load_$(TARGET)_param.sh
 	tar czvf $@ -C $(BUILD_DIR) $(TARGET)
 
