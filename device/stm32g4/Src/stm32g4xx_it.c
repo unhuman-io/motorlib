8185acae84a9c6e3985324da1cc37b731a282de2
leemagnusson <leemagnusson@gmail.com>
2024-03-19 18:18:53 -0700

    Revert "Feature/hrtim mainloop (#222)" (#223)
    This reverts commit 2eedcdb6c2b0a39f4ff83f5867a689e22aea8266.

--- device/stm32g4/Src/stm32g4xx_it.c
+++ device/stm32g4/Src/stm32g4xx_it.c
@@ -100,17 +100,7 @@ void ADC5_IRQHandler(void)
   SET_SCOPE_PIN(C,1);
   INTERRUPT_PROFILE_START;
   fast_loop_interrupt();
-  ADC5->ISR = ADC_ISR_JEOS | ADC_ISR_JEOC;
+  ADC5->ISR = ADC_ISR_JEOS;
   INTERRUPT_PROFILE_END(fastloop)
   CLEAR_SCOPE_PIN(C,1);
-}
-
-void HRTIM1_Master_IRQHandler(void)
-{
-  SET_SCOPE_PIN(C,0);
-  INTERRUPT_PROFILE_START;
-  main_loop_interrupt();
-  HRTIM1->sMasterRegs.MICR = HRTIM_MICR_MCMP1;
-  INTERRUPT_PROFILE_END(mainloop);
-  CLEAR_SCOPE_PIN(C,0); 
 }
