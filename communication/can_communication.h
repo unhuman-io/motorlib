b9682dec3fedc97bf638c1da464a860032246b5a
Lee Magnusson <lee@unhuman.io>
2024-10-07 15:09:34 -0700

    can long packets and timeout

--- communication/can_communication.h
+++ communication/can_communication.h
@@ -82,14 +82,38 @@ class CANCommunication : public CommunicationBase {
 
     bool send_string(const char* string, uint16_t length) {
       CANID can_id = {.address = address_, .message_id = OBOT_ASCII_RESPONSE};
-      length = std::min(length, (uint16_t) 63);
-      char buf[64];
-      std::memcpy(buf, string, length);
-      if (length > 1) {
-        buf[length++] = 0;
+      if (string[0] == 0 || length > MAX_CAN_DATA_SIZE) {
+        struct {
+          APIControlPacket control_packet = {0, LONG_PACKET, .long_packet = {0, 1}};
+          char data[MAX_CAN_DATA_SIZE - sizeof(APIControlPacket)];
+        } long_packet;
+        long_packet.control_packet.long_packet.total_length = length;
+        int32_t length_remaining = length;
+        const char * str = string;
+        do {
+          uint16_t transfer_size = std::min((uint16_t) (MAX_CAN_DATA_SIZE - sizeof(APIControlPacket)), (uint16_t) length_remaining);
+          std::memcpy(long_packet.data, str, transfer_size);
+          can_.write(can_id.word, (uint8_t * const) &long_packet, 
+                  transfer_size + sizeof(APIControlPacket));
+          str += transfer_size;
+          long_packet.control_packet.long_packet.packet_number++;
+          length_remaining -= transfer_size;
+        } while (length_remaining > 0);
+      } else {
+        char buf[64];
+        std::memcpy(buf, string, length);
+        if (length > 1) {
+          buf[length++] = 0;
+        }
+        can_.write(can_id.word, (uint8_t*) buf, length);
       }
-      can_.write(can_id.word, (uint8_t*) buf, length);
       return true;
+    }
+
+    void send_one_time_api_timeout_request(uint32_t us) {
+       APIControlPacket timeout_request = {0, TIMEOUT_REQUEST, .timeout_request = {us}};
+       CANID can_id = {.address = address_, .message_id = OBOT_ASCII_RESPONSE};
+       can_.write(can_id.word, (uint8_t * const) &timeout_request, sizeof(timeout_request));
     }
 
  private:
