a6a323217c6433ae031615759e3acc40b39d34e6
leemagnusson <leemagnusson@gmail.com>
2024-10-10 09:58:14 -0700

    Feature/update gcc (#277)
    * gcc 13.3
    
    * add spi4 dma
    
    ---------
    
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
-docker build . -t leemagnusson/arm-gcc:latest
-docker push leemagnusson/arm-gcc:latest
+tagname=13.3
+
+docker build . -t leemagnusson/arm-gcc:$tagname
+docker tag leemagnusson/arm-gcc:$tagname leemagnusson/arm-gcc:latest
+docker push leemagnusson/arm-gcc:$tagname #leemagnusson/arm-gcc:latest
