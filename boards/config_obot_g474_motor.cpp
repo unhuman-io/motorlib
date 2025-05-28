a1a873b8dcb4726bab46921be5440b243d618fdb
leemagnusson <leemagnusson@gmail.com>
2025-05-28 15:59:25 -0700

    Feature/6enc (#317)
    * fix some led frequencies
    
    * timeout on mgt request
    
    ---------
    
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

--- boards/config_obot_g474_motor.cpp
+++ boards/config_obot_g474_motor.cpp
@@ -312,7 +312,8 @@ namespace config {
 
     LED led = {const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(get_board_pins(board_rev).led_tim_r)), 
                const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(get_board_pins(board_rev).led_tim_g)),
-               const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(get_board_pins(board_rev).led_tim_b))};
+               const_cast<uint16_t*>(reinterpret_cast<volatile uint16_t *>(get_board_pins(board_rev).led_tim_b)),
+               config::main_loop_frequency};
     volatile uint32_t &V5V_DR = *get_board_pins(board_rev).v5v_dr;
 #ifndef POSITION_CONTROLLER_OVERRIDE
     PositionController position_controller = {(float) (1.0/main_loop_frequency)};
