1c1cfb943e7e293535a7582207cd9e3ed238ddfe
leemagnusson <leemagnusson@gmail.com>
2025-07-30 16:33:44 -0700

    Feature/c++20 (#322)
    * c++20
    
    * install newlib overlay
    
    * can packet initialization

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -8,7 +8,8 @@ cd $llvm_dir
 
 version=20.1.0
 declare -A sha=( [x86_64]=c1179396608c07bf68f3014923cfdfcd11c8402a3732f310c23d07c9a726b275 \
-                 [aarch64]=2fa9220f64097b71c07e6de2917f33fda1bb736964730786e90a430fdc0fa6be )
+                 [aarch64]=2fa9220f64097b71c07e6de2917f33fda1bb736964730786e90a430fdc0fa6be \
+                 [newlib]=2e649e17b161b81f0ddf5361dc8f648d66f9c1cf9665c6b8a621d44aef51ad7d)
 
 arch=$(uname -m)
 if [ "$arch" == "aarch64" ]; then
@@ -16,10 +17,21 @@ if [ "$arch" == "aarch64" ]; then
 else
   farch=x86_64
 fi
-fname=ATfE-$version-Linux-$farch
-url=https://github.com/arm/arm-toolchain/releases/download/release-$version-ATfE/$fname.tar.xz
-wget $url -O $fname.tar.xz
-cat <(printf "${sha[$arch]} $fname.tar.xz\n")
-sha256sum -c <(printf "${sha[$arch]} $fname.tar.xz\n")
-tar xf $fname.tar.xz --strip-components=1
-rm $fname.tar.xz
+
+fnames=(ATfE-$version-Linux-$farch \
+        ATfE-newlib-overlay-$version)
+sha_keys=($arch newlib)
+for i in "${!fnames[@]}"; do
+  fname=${fnames[$i]}
+  url=https://github.com/arm/arm-toolchain/releases/download/release-$version-ATfE/$fname.tar.xz
+  wget $url -O $fname.tar.xz
+  cat <(printf "${sha[${sha_keys[$i]}]} $fname.tar.xz\n")
+  sha256sum -c <(printf "${sha[${sha_keys[$i]}]} $fname.tar.xz\n")
+  if [ $i -eq 0 ]; then
+    tar xf $fname.tar.xz --strip-components=1
+  else
+    tar xf $fname.tar.xz
+  fi
+  
+  #rm $fname.tar.xz
+done
