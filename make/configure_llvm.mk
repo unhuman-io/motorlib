6f1c918161572f819ea96735d8cb097d4a471f2f
Lee Magnusson <lee.magnusson@figure.ai>
2025-09-02 14:35:41 -0700

    cleaner clang build

--- make/configure_llvm.mk
+++ make/configure_llvm.mk
@@ -44,7 +44,7 @@ ASFLAGS = $(MCU) $(AS_DEFS) $(AS_INCLUDES) $(OPT) -Wall -fdata-sections -ffuncti
 CFLAGS = $(MCU) $(C_DEFS) $(C_INCLUDES) $(OPT) -Wall -fdata-sections -ffunction-sections $(LTO) -Wno-vla-cxx-extension -Wno-c99-designator -Wno-c23-extensions
 
 ifeq ($(DEBUG), 1)
-CFLAGS += -g -gdwarf-2
+CFLAGS += -g3
 endif
 
 # Generate dependency information
