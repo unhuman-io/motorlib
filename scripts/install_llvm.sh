547df70a37b9aa52929af0f5e27227a978db5da8
Lee Magnusson <lee.magnusson@figure.ai>
2026-01-05 12:07:31 -0800

    add clang21

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -6,10 +6,15 @@ llvm_dir=$(dirname $0)/../llvm
 mkdir -p $llvm_dir
 cd $llvm_dir
 
-version=20.1.0
-declare -A sha=( [x86_64]=c1179396608c07bf68f3014923cfdfcd11c8402a3732f310c23d07c9a726b275 \
-                 [aarch64]=2fa9220f64097b71c07e6de2917f33fda1bb736964730786e90a430fdc0fa6be \
-                 [newlib]=2e649e17b161b81f0ddf5361dc8f648d66f9c1cf9665c6b8a621d44aef51ad7d)
+# version=20.1.0
+# declare -A sha=( [x86_64]=c1179396608c07bf68f3014923cfdfcd11c8402a3732f310c23d07c9a726b275 \
+#                  [aarch64]=2fa9220f64097b71c07e6de2917f33fda1bb736964730786e90a430fdc0fa6be \
+#                  [newlib]=2e649e17b161b81f0ddf5361dc8f648d66f9c1cf9665c6b8a621d44aef51ad7d)
+
+version=21.1.1
+declare -A sha=( [x86_64]=fd7fcc2eb4c88c53b71c45f9c6aa83317d45da5c1b51b0720c66f1ac70151e6e \
+                 [aarch64]=dfd93d7c79f26667f4baf7f388966aa4cbfd938bc5cbcf0ae064553faf3e9604 \
+                 [newlib]=d9750863c5561c05a57f6df6019efea87e9206c0eef34c4e6441f339824cc908)
 
 arch=$(uname -m)
 if [ "$arch" == "aarch64" ]; then
