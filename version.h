cc70e56e5d1069dea01bdf5d80a1432795e81ec1
Lee Magnusson <lee@unhuman.io>
2026-03-30 17:59:29 -0700

    wip cmake

---
+++ version.h
@@ -0,0 +1,9 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define BUILD_DATETIME __DATE__ " " __TIME__
+#define PRODUCT_STRING "OBOT motor controller"
+#define USBD_PID (0x100)
+#define MANUFACTURER_STRING "Unhuman Inc."
+
+#endif
