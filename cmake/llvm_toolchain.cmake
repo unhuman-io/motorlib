001fe99f3a4a211a6b72e20c19502e6cd59d7da6
Lee Magnusson <lee@unhuman.io>
2026-04-03 06:46:09 -0700

    move build functions to motorlib

--- cmake/llvm_toolchain.cmake
+++ cmake/llvm_toolchain.cmake
@@ -9,7 +9,7 @@ set(TARGET_TRIPLE "armv7em-none-eabihf")
 set(MCU_FLAGS "-mcpu=cortex-m4" "-mfpu=fpv4-sp-d16" "-mfloat-abi=hard")
 
 # This is the "secret sauce" from your Makefile
-set(COMMON_FLAGS "--target=${TARGET_TRIPLE}" "${MCU_FLAGS}" "-fdata-sections" "-ffunction-sections")
+set(COMMON_FLAGS "--target=${TARGET_TRIPLE}" "${MCU_FLAGS}")
 
 set(CMAKE_C_COMPILER "${BIN_DIR}/clang")
 set(CMAKE_CXX_COMPILER "${BIN_DIR}/clang++")
@@ -20,7 +20,7 @@ add_link_options(${COMMON_FLAGS})
 add_compile_options($<$<COMPILE_LANGUAGE:ASM>:-x> $<$<COMPILE_LANGUAGE:ASM>:assembler-with-cpp>)
 
 # Match your Makefile LDFLAGS exactly
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostartfiles -lc -lm -lnosys -Wl,--gc-sections -Wl,--defsym=vfprintf=__f_vfprintf")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostartfiles -lc -lm -lnosys -Wl,--defsym=vfprintf=__f_vfprintf")
 
 # 5. Cross-Compilation Search Behavior
 # Prevent CMake from accidentally linking against your Pop!_OS desktop libraries
