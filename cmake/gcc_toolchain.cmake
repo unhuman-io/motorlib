e8142643115aaa6e4184a9dfbf1eede2a6b4d2a9
Lee Magnusson <lee@unhuman.io>
2026-04-04 05:42:37 -0700

    some cleanup

--- cmake/gcc_toolchain.cmake
+++ cmake/gcc_toolchain.cmake
@@ -1,26 +1,21 @@
-# 1. Target OS and Architecture
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# 2. Compilers
 set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
-set(CMAKE_ASM_COMPILER "arm-none-eabi-g++") # Let GCC handle the .s files
+set(CMAKE_ASM_COMPILER "arm-none-eabi-g++")
 
-# 3. Hardware-Specific Compiler Flags
-# Define the common flags once so you don't repeat yourself
+# Hardware-Specific Compiler Flags
 set(MCU_FLAGS "-mcpu=cortex-m4" "-mfpu=fpv4-sp-d16" "-mfloat-abi=hard")
 
+# Linker Flags
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=nosys.specs -u _printf_float")
 
-# 4. Linker Flags
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=nosys.specs")
-
-# 5. Cross-Compilation Search Behavior
-# Prevent CMake from accidentally linking against your Pop!_OS desktop libraries
+# Cross-Compilation Search Behavior
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-# Force CMake to look in your specific folder first for objcopy, size, ar, etc.
+# Force CMake to look in specific folder first for objcopy, size, ar, etc.
 set(CMAKE_FIND_ROOT_PATH "${CMAKE_CURRENT_LIST_DIR}/motorlib/gcc")
--- cmake/llvm_toolchain.cmake
+++ cmake/llvm_toolchain.cmake
@@ -1,4 +1,3 @@
-# 1. Target OS and Architecture
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
@@ -8,24 +7,21 @@ set(BIN_DIR "${LLVM_ROOT}/bin")
 set(TARGET_TRIPLE "armv7em-none-eabihf")
 set(MCU_FLAGS "--target=${TARGET_TRIPLE}" "-mcpu=cortex-m4" "-mfpu=fpv4-sp-d16" "-mfloat-abi=hard")
 
-
 set(CMAKE_C_COMPILER "${BIN_DIR}/clang")
 set(CMAKE_CXX_COMPILER "${BIN_DIR}/clang++")
 set(CMAKE_ASM_COMPILER "${BIN_DIR}/clang")
 
 add_compile_options($<$<COMPILE_LANGUAGE:ASM>:-x> $<$<COMPILE_LANGUAGE:ASM>:assembler-with-cpp>)
 
-# Match your Makefile LDFLAGS exactly
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostartfiles -lc -lm -lnosys -Wl,--defsym=vfprintf=__f_vfprintf")
 
-# 5. Cross-Compilation Search Behavior
-# Prevent CMake from accidentally linking against your Pop!_OS desktop libraries
+# Cross-Compilation Search Behavior
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-# Force CMake to look in your specific folder first for objcopy, size, ar, etc.
+# Force CMake to look in specific folder first for objcopy, size, ar, etc.
 set(CMAKE_FIND_ROOT_PATH "${LLVM_BIN_DIR}")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
