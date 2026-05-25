fbe890ec96472198f134002d2a59329efae4d743
leemagnusson <leemagnusson@gmail.com>
2026-05-25 06:45:49 -0700

    move async logic to generic communication (#352)

--- communication.h
+++ communication.h
@@ -2,6 +2,8 @@
 #define UNHUMAN_MOTORLIB_COMMUNICATION_H_
 
 #include "messages.h"
+#include "task.h"
+#include <cstring>
 
 class CommunicationBase {
  public:
@@ -41,6 +43,51 @@ class CommunicationBase {
     // round robin data
     bool tx_data_ack() {
       return false;
+    }
+
+    // requires that derived class supports write_async
+    template <typename Self>
+    requires requires(Self& s, Scheduler& sched, uint8_t* data, uint16_t len) {
+        { s.write_async(sched, data, len) } -> std::same_as<Task<int>>;
+    }
+    Task<bool> send_string_async(this Self&& self, Scheduler &sched, const char* string, uint16_t length) {
+      using ActualSelf = std::decay_t<Self>;
+      constexpr uint32_t MAX_SIZE = ActualSelf::MAX_PACKET_TRANSFER_SIZE;
+      constexpr uint32_t MAX_PAYLOAD = MAX_SIZE - sizeof(APIControlPacket);
+
+     // if string is binary (starts with 0? or is long, send as a series of chunks
+      if (length && (string[0] == 0 || length > MAX_SIZE - 1)) {
+        struct {
+          APIControlPacket control_packet = {.control_packet_id = 0,
+                                             .type = LONG_PACKET,
+                                             .long_packet = {0, 1}};
+          char data[MAX_PAYLOAD];
+        } long_packet;
+        long_packet.control_packet.long_packet.total_length = length;
+        int32_t length_remaining = length;
+        const char * str = string;
+        do {
+          uint16_t transfer_size = std::min((uint16_t) MAX_PAYLOAD, (uint16_t) length_remaining);
+          std::memcpy(long_packet.data, str, transfer_size);
+          int retval = co_await self.write_async(sched, (uint8_t * const) &long_packet, 
+                  transfer_size + sizeof(APIControlPacket));
+          if (retval < 0) {
+            co_return false;
+          }
+          str += transfer_size;
+          long_packet.control_packet.long_packet.packet_number++;
+          length_remaining -= transfer_size;
+        } while (length_remaining > 0);
+      } else {
+        char buf[64];
+        std::memcpy(buf, string, length);
+        if (length > 1) {
+          buf[length++] = 0;
+        }
+        int retval = co_await self.write_async(sched, (uint8_t*) buf, length);
+        co_return retval >= 0;
+      }
+      co_return true;
     }
 };
 
